a68ad26e8144e0c20de41c2a824c41cb84ce3d9d
{Trucdeeptry} <{phantruc438@gmail.com}>
2024-03-25 20:15:40 +0700

    fix redirect index

--- Sign-upform/main.js
+++ Sign-upform/main.js
@@ -43,7 +43,7 @@ function handleCredentialResponse(response) {
     googleButton.style.display = 'none'
     isLoggedIn = true;
     SetSession();
-    window.location.href = "/index.html"
+    window.location.href = "../index.html"
 }
 
 window.onload = function () {
